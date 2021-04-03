5877d7dcd4f5254918a51124ffd24b4b1fcc98bf
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-03 14:31:38 -0700

    defined schema

---
+++ database/model/queryFunctions.js
@@ -0,0 +1,1 @@
+const { User, Room } = require('./schema.js');
---
+++ database/model/schema.js
@@ -0,0 +1,19 @@
+const mongoose = require('../index.js');
+
+const roomSchema = mongoose.Schema({
+  userOneID: String,
+  userTwoID: String,
+  messages: Array
+});
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  bio: String,
+  country: String,
+  pendingConnections: Object, // object of userIds as keys and 0, 1, or 2 as value
+  rooms: Object // object of roomIds and value as userID of connection
+});
+
+module.exports.Room = mongoose.model('Room', roomSchema);
+module.exports.User = mongoose.model('User', roomSchema);
