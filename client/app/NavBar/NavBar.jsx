672a12b19670dd9f6662d1a119767f85e4cdd49d
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-04-03 16:07:46 -0600

    Nav bar CSS

--- client/app/NavBar/NavBar.jsx
+++ client/app/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
-     <span className="nav-title">CORRESPOND</span>
-     <span className="nav-connection">  +  </span>
-     <span className="nav-notifications">BellIconHere</span>
-     <span className="nav-profile">ProfilePicHere</span>
+      <span className="nav-title">CORRESPOND</span>
+      <span className="icon-container">
+        <span className="nav-connection"><ion-icon name="add-circle-outline"></ion-icon></span>
+        <span className="nav-notifications"><ion-icon name="notifications-circle-outline"></ion-icon></span>
+        <span className="nav-profile"><ion-icon name="ellipse-outline"></ion-icon></span>
+      </span>
     </div>
   )
 }
