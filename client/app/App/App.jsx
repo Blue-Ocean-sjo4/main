80535975847d5644846b635b940eb32af2931409
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-04-05 16:12:11 -0600

    fixed login CSS

--- client/app/App/App.jsx
+++ client/app/App/App.jsx
@@ -8,9 +8,9 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      {/* <Login /> */}
+      <Login />
       {/* <SignUp /> */}
-      <ProfilePage />
+      {/* <ProfilePage /> */}
     </div>
   )
 }
--- client/app/NavBar/NavBar.jsx
+++ client/app/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div className="nav-bar">
       <span className="nav-title">CORRESPOND</span>
       <span className="icon-container">
-        <span className="nav-connection"><ion-icon name="add-circle-outline"></ion-icon></span>
+        <span className="nav-new-pal"><ion-icon name="add-circle-outline"></ion-icon></span>
         <span className="nav-notifications"><ion-icon name="notifications-circle-outline"></ion-icon></span>
         <span className="nav-profile"><ion-icon name="ellipse-outline"></ion-icon></span>
       </span>
