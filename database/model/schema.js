b49ce02363d30fbb591a4eb71025c98069b5bd0d
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-05 15:53:59 -0700

    updated User Schema

--- database/model/schema.js
+++ database/model/schema.js
@@ -9,13 +9,16 @@ const roomSchema = mongoose.Schema({
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
+  // profile picture
   email: String,
   bio: String,
   country: String,
+  gender: String,
+  pronouns: String,
   birthdate: Date,
   pendingConnections: Object, // object of userIds as keys and 0, 1, or 2 as value
   rooms: Object // object of roomIds and value as userID of connection
 });
 
 module.exports.Room = mongoose.model('Room', roomSchema);
-module.exports.User = mongoose.model('User', roomSchema);
+module.exports.User = mongoose.model('User', userSchema);
