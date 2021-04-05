dcc6723b2325fc1219b6c2f879c7a52c42c85ae7
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-05 15:53:25 -0700

    updated signup logic

--- database/model/queryFunctions.js
+++ database/model/queryFunctions.js
@@ -1,7 +1,8 @@
 const { User, Room } = require('./schema.js');
 const bcrypt = require('bcrypt');
 
-module.exports.signup = async (response, request) => {
+module.exports.signup = async (request, response) => {
+  console.log('request body:', request.body);
   const { username, password, email, country, birthdate } = request.body;
   try {
     const doesExist = await User.exists({ username });
@@ -10,8 +11,9 @@ module.exports.signup = async (response, request) => {
       response.sendStatus(409);
     } else {
       const hashPassword = await bcrypt.hash(password, 10);
-      await User.create({username, hashPassword, email, country, birthdate});
-      response.redirect('/login');
+      await User.create({ username, password: hashPassword, email, country, birthdate });
+      // response.redirect('/login');
+      response.send('registered');
     }
   } catch (error) {
     console.error(error);
@@ -25,7 +27,7 @@ module.exports.findID = (id) => User.findById({ _id: id });
 
 module.exports.findUserData = async (req, res) => {
   try {
-    const loggedInUser = await User.findOne({ _id: id });
+    const loggedInUser = await User.findOne({ _id: req.body.id });
     const rooms = Object.entries(loggedInUser.rooms);
     const roomsPayload = rooms.map(async (room) => {
       const connection = await User.findOne({ _id: room[1] });
@@ -56,9 +58,13 @@ module.exports.findUserData = async (req, res) => {
 
     res.send(loggedInUser);
   } catch (error) {
-    res.status(404).send(err);
+    res.status(404).send(error);
     console.error(error);
   };
+};
+
+module.exports.updateUserData = (request, response) => {
+
 };
 
 //const hashPassword = await bcrypt.hash(<password>, 10);
