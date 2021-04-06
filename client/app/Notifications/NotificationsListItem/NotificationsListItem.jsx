8458bf978a157f764bd941dc9ffbc92149c83c40
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-04-06 11:39:31 -0600

    notification page structure

---
+++ client/app/Notifications/NotificationsListItem/NotificationsListItem.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import countryCodeToFlag from "country-code-to-flag";
+import './NotificationsListItem.css';
+
+const NotificationsListItem = ({ name, bio, country }) => {
+
+  // TODO: button handlers
+
+  return (
+    <div className="connection-container">
+      <div className="connection-item-picture-container">
+        <img className="connection-item-picture" src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" alt=""></img>
+      </div>
+      {/* <img className="connection-item-picture" src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" alt=""></img> */}
+      <div className="connection-item-name">{name}</div>
+      <div className="connection-item-bio">{bio}</div>
+      <div className="connection-item-country">{countryCodeToFlag(country)}
+      </div>
+      {/* TODO: add buttons for accept or deny here */}
+    </div>
+  )
+}
+
+export default NotificationsListItem;
