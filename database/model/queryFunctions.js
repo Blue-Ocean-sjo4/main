5bfeebf8b275074c208ac1f124a0648262342acd
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-06 11:41:28 -0700

    made progress on findPal logic

--- database/model/queryFunctions.js
+++ database/model/queryFunctions.js
@@ -1,5 +1,6 @@
-const { User, Room } = require('./schema.js');
+const { User, Room, Test } = require('./schema.js');
 const bcrypt = require('bcrypt');
+const moment = require('moment');
 
 /*
 *-----------------------------------------------------------*
@@ -19,7 +20,10 @@ function calculate_age(dob) {
 module.exports.signup = async (request, response) => {
   console.log('request body:', request.body);
   const { username, password, email, country, birthdate } = request.body;
-  const birthDate = new Date(birthdate.subString(0,3), birthdate.subString(5,6), birthdate.subString(8,9));
+  console.log(birthdate);
+  const birthDate = new Date(moment(birthdate));
+
+  // const birthDate = new Date(birthdate.subString(0,3), birthdate.subString(5,6), birthdate.subString(8,9));
   try {
     const doesExist = await User.exists({ username });
 
@@ -152,8 +156,10 @@ module.exports.postMessage = async (request, response) => {
 };
 
 module.exports.findPal = async (request, response) => {
-  const { user_id, country, birthdate } = request.query;
+  console.log(1);
+  const { user_id, country } = request.params;
   // find user by user_id
+  console.log('country data', country);
   const userData = await User.findOne({ _id: user_id });
   const ineligiblePals = [];
   // loop through pending;
@@ -162,23 +168,52 @@ module.exports.findPal = async (request, response) => {
   Object.values(userData.rooms).forEach(user => ineligiblePals.push(user));
   // user is requesting someone to be a pal
   // requestedConnections
+  const userBirthDate = userData.birthdate;
+  const legalAge = new Date(moment().subtract(18, 'years'));
 
+  let bdayCriteria = moment(userBirthDate) >= legalAge ? { $gte: legalAge } : { $lt: legalAge };
+  console.log('birthday criteria', bdayCriteria);
+  // TODO: randomize before limit
+  const randomPal = await User.aggregate([
+    {
+      $match: {
+        _id: { $nin: ineligiblePals },
+        country: country,
+        birthdate: bdayCriteria
+      },
+    },
+    {
+      $sample: { size: 1 }
+    }
+  ]);
 
-  //18
-  // const bdayCriteria
-  // { $gte: value }
-  // <18
-  // { $lt: value }
-  // TODO: implement milisecond filter for age group
+  console.log('random pal', randomPal);
 
-  const possiblePals = await User.find({ _id: { $nin: ineligiblePals }, country: country, birthdate: bdayCriteria })
-  // const getPhotos = (reviewIds) => ReviewsPhotos.find({ review_id: { $in: reviewIds } })
+  randomPal[0].pendingConnections[user_id] = 0;
+  await randomPal.save();
+
+  response.sendStatus(200);
+  // const getAggregatedRatings = (reviewIds) => Reviews
+  // .aggregate([
+  //   { $match: { review_id: { $in: reviewIds } } },
+  //   { $group: { _id: '$rating', count: { $sum: 1 } } },
+  // ])
+  // ;
+
+  // const randomPal = possiblePals[Math.floor(Math.random() * possiblePals.length)];
+
 };
-// db.movies.update({'seasons.episodes.videos._id': data._id}, {$push: {'seasons.0.episodes.0.videos.$.reports': data.details}})
 
+//db.mycoll.aggregate([{ $sample: { size: N } }])
 //const hashPassword = await bcrypt.hash(<password>, 10);
 
-
+module.exports.test = async (req, res) => {
+  await Test.create({ testDate: moment() });
+  const results = await Test.find({});
+  const testDate = results[0].testDate;
+  console.log(new Date(moment('1991-04-05')));
+  res.sendStatus(201);
+};
 
 
 
