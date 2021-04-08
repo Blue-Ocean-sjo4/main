d97173601673dbcc793e9e1dd93a72b784e16023
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-04-08 13:17:04 -0600

    database IP config added to gitignore

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { DB_IP } = require('../config.js');
 // mongoose.connect('mongodb://localhost:27017/testBO', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect('mongodb://54.176.37.47/testBO', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb://${DB_IP}/testBO`, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Mongoose connected'))
   .catch(() => console.log('connection fails'));
 
