7d246b764e9ca15b7c4379d89a3d5218bd3c55dd
Evan <evanjordan42@gmail.com>
2021-04-08 10:43:58 -0700

    Improved new pal modal styling

--- client/app/NavBar/NewPal/NewPal.jsx
+++ client/app/NavBar/NewPal/NewPal.jsx
@@ -8,6 +8,7 @@ import randomCountry from 'random-country';
 function NewPal({ showingNewPal, setShowingNewPal, userID }) {
 
   const [country, setCountry] = useState('');
+  const [showingConfirmation, showConfirmation] = useState(false)
 
   function handleRegionClick(e, name) {
     if (country === name) {
@@ -19,6 +20,8 @@ function NewPal({ showingNewPal, setShowingNewPal, userID }) {
 
   function closeModal() {
     setShowingNewPal(false)
+    showConfirmation(false)
+    setCountry('')
     var elements = document.getElementsByClassName('jvectormap-tip');
     for (var element of elements) {
       element.style.display = 'none';
@@ -30,12 +33,10 @@ function NewPal({ showingNewPal, setShowingNewPal, userID }) {
     var sendingCountry = country
     if (!country) {
       sendingCountry = randomCountry();
-      console.log(sendingCountry)
     }
     e.preventDefault();
     axios.post(`/newPal/${userID}/${sendingCountry}`)
-      .then(() => { alert('Pal request sent!') })
-      .then(() => { setShowingNewPal(false) })
+      .then(() => { showConfirmation(true) })
       .catch((err) => { alert('error adding new pal, check console'); console.log('err: ', err) })
   }
 
@@ -46,7 +47,7 @@ function NewPal({ showingNewPal, setShowingNewPal, userID }) {
           <div id="newPal" className="new-pal-modal">
             <div className="new-pal-modal-content">
               <span className="new-pal-modal-close" onClick={closeModal} >&times;</span>
-              Choose the country of your new pal! Or, select none and a random country will be selected instead!
+              <div className="new-pal-prompt">Choose the country of your new pal! Or, select none and a random country will be selected instead!</div>
               <div id="new-pal-map" className="map">
                 <div style={{ width: 1000, height: 500 }}>
                   <VectorMap map={'world_mill'}
@@ -62,7 +63,16 @@ function NewPal({ showingNewPal, setShowingNewPal, userID }) {
               {
                 country ? <div className="current-country-flag">{countryCodeToFlag(country)}</div> : <div className="current-country-flag"></div>
               }
-              <button onClick={addPal}>Send Pal Request</button>
+              <button className="send-pal-request-button" onClick={addPal}>Send Pal Request</button>
+              {
+                showingConfirmation ?
+                  <div className="new-pal-confirmation-modal">
+                    <div className="new-pal-confirmation-modal-content">
+                      Pal request sent!
+                      <div><button className="new-pal-confirmation-button" onClick={closeModal}>Ok</button></div>
+                    </div>
+                  </div> : null
+              }
             </div>
           </div> : null
       }
