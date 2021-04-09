71c98aa23e8964f0c4b5506357f9d0a263d7c838
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-08 23:03:58 -0700

    - updated the db name from testBO to blueocean - added an auth property to the connection config

--- database/index.js
+++ database/index.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 const { DB_IP } = require('../config.js');
 // mongoose.connect('mongodb://localhost:27017/testBO', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect(`mongodb://${DB_IP}/testBO`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(
+  `mongodb://${DB_IP}/blueocean`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    auth: {
+      authSource: 'admin'
+    }
+  })
   .then(() => console.log('Mongoose connected'))
-  .catch(() => console.log('connection fails'));
+  .catch((error) => console.log('connection fails'));
 
 module.exports = mongoose;
