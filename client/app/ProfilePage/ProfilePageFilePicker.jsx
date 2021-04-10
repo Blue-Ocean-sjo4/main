00408b34db2fc12796ab240fbb3762b78e30a741
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-10 12:53:24 -0700

    debugging TOKEN for filestack

--- client/app/ProfilePage/ProfilePageFilePicker.jsx
+++ client/app/ProfilePage/ProfilePageFilePicker.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { PickerOverlay } from 'filestack-react';
 // import { TOKEN } from '../../../filepickerConfig.js';
 const client = require('filestack-js').init(process.env.TOKEN);
-
+const TOKEN = process.env.TOKEN;
+console.log('token', TOKEN);
 const ProfilePageFilePicker = ({ profilePicture, setProfilePicture }) => {
   // TODO add profile page state here (profilePic and SetProfilePic)
   return (
     <PickerOverlay
-      apikey={process.env.TOKEN}
+      apikey={TOKEN}
       onSuccess={
         (res) => {
           console.log('res: ', res)
