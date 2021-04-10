0d5ab42725c7df7923c27adaeadfb0dd8fad336a
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-10 10:48:10 -0700

    updated connection string to the dotenv variable

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const { DB_IP } = require('../config.js');
+// const { DB_IP } = require('../config.js');
 mongoose.connect(
-  `mongodb+srv://${DB_IP}/blueocean`,
+  // `mongodb+srv://${DB_IP}/blueocean`,
+  `mongodb+srv://${process.env.DB_IP}/blueocean`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
