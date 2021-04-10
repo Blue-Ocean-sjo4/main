d525d5f35e20687b52e5671266adda83920a1ecf
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-10 10:25:10 -0700

    updated connection string

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const { DB_IP } = require('../config.js');
 mongoose.connect(
-  // `mongodb://${DB_IP}/blueocean`,
   `mongodb+srv://${DB_IP}/blueocean`,
   {
     useNewUrlParser: true,
